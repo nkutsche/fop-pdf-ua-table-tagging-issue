9f389e27c48776169babf7878ada713a99216fbd
Nico Kutscherauer <kutscherauer@data2type.de>
2024-02-02 11:42:19 +0100

    Adds sample generating PDF 1.4

--- make.bat
+++ make.bat
@@ -2,3 +2,5 @@
 call mvn exec:java -Dexec.mainClass=org.apache.fop.cli.Main -Dexec.args="-c config/fop-config-ua.xml -fo table.fo -pdf %CD%/out/table-ua.pdf"
 
 call mvn exec:java -Dexec.mainClass=org.apache.fop.cli.Main -Dexec.args="-c config/fop-config.xml -fo table.fo -pdf %CD%/out/table.pdf"
+
+call mvn exec:java -Dexec.mainClass=org.apache.fop.cli.Main -Dexec.args="-c config/fop-config14.xml -fo table.fo -pdf %CD%/out/table14.pdf"
